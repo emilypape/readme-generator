333b0e32e1902ca99e07320f4129d81c799d4fa6
emily <eap6787@gmail.com>
2022-03-25 14:06:05 -0600

    install and test inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,23 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = inquirer.prompt([
+    {
+        type: 'input',
+        name: 'project name',
+        message: 'What is the title of your project?',
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your project title!')
+            }
+        }
+    }
+])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
